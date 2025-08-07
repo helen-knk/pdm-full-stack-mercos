dd0f15a926a5a348b0bc20a58b2b6601e7042aeb
Helen Christine <helen.christine.kds@gmail.com>
2025-08-06 22:39:58 -0300

    Desafio 01: Não permite digitar números inválidos

--- 01/script.py
+++ 01/script.py
@@ -1,11 +1,20 @@
 import datetime
 
-nome = input('Digite seu nome: ')
-idade = int(input('Digite sua idade: '))
-
 def calcular_ano_100(idade):
     ano_atual = datetime.datetime.now().year
     return ano_atual + (100 - idade)
+
+nome = input('Digite seu nome: ')
+
+while True:
+    try:
+        idade = int(input('Digite sua idade: '))
+        if 0 >= idade or idade >= 120:
+            print('Idade inválida. (intervalo de 0 ao 120).')
+        else:
+            break
+    except ValueError:
+        print('OPA! Escreva uma idade válida.')
 
 ano_100 = calcular_ano_100(idade)
 
